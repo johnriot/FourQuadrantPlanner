8f511eb71565f6fe35c5485da560bd182858bf10
John O'Halloran <ohalloran.john@gmail.com>
2014-04-23 15:16:08 -0300

    Made hint text look nice and disappear on focus. Fixed focus issue. Adjusted colours slightly. Fixed .gitignore file problem.

--- gen/com/example/fourquadrantplanner/R.java
+++ gen/com/example/fourquadrantplanner/R.java
@@ -24,7 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int bottom_left_editText=0x7f080002;
+        public static final int bottom_right_editText=0x7f080003;
+        public static final int top_left_editText=0x7f080000;
+        public static final int top_right_editText=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -39,6 +43,7 @@ public final class R {
         public static final int fourth_quadrant_heading=0x7f050005;
         public static final int second_quadrant_heading=0x7f050003;
         public static final int third_quadrant_heading=0x7f050004;
+        public static final int to_do_hint_text=0x7f050006;
     }
     public static final class style {
         /** 
--- src/com/example/fourquadrantplanner/MainActivity.java
+++ src/com/example/fourquadrantplanner/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.fourquadrantplanner;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,47 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		final EditText tlTodoBox = (EditText) findViewById(R.id.top_left_editText);
+		final EditText trTodoBox = (EditText) findViewById(R.id.top_right_editText);
+		final EditText blTodoBox = (EditText) findViewById(R.id.bottom_left_editText);
+		final EditText brTodoBox = (EditText) findViewById(R.id.bottom_right_editText);
+
+		tlTodoBox.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					tlTodoBox.setHint("");
+				}
+			}
+		});
+
+		trTodoBox.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					trTodoBox.setHint("");
+				}
+			}
+		});
+
+		blTodoBox.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					blTodoBox.setHint("");
+				}
+			}
+		});
+
+		brTodoBox.setOnFocusChangeListener(new OnFocusChangeListener() {
+			@Override
+			public void onFocusChange(View v, boolean hasFocus) {
+				if (hasFocus) {
+					brTodoBox.setHint("");
+				}
+			}
+		});
 	}
 
 	@Override
